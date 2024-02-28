f2d16d34bdca0a460c2aa78f9d3afa8f86e7fe3b
sisdream <sissipascale2@gmail.com>
2024-02-28 16:50:42 +0100

    feat: ciclo for

--- function.php
+++ function.php
@@ -4,7 +4,22 @@ function getRandomString($n)
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,-_!?^|@#%$£=';
     $randomString = '';
 
-
+    $checked = '';
+    if (isset($_GET['repeat'])) {
+        $checked = 'checked';
+    }
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        if ($checked) {
+            if (str_contains($randomString, $index)) {
+                return;
+            } else {
+                $randomString .= $characters[$index];
+            }
+        } else {
+            $randomString .= $characters[$index];
+        }
+    }
     return $randomString;
 }
 
